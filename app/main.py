c791a43beba95ecb2c1dae8f0098cec90f7b752b
Caroline Mimeault <carolinemimeault@gmail.com>
2023-02-18 22:01:40 -0500

    add graphql-api hello world

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
+import strawberry
+
 from fastapi import FastAPI
+from strawberry.fastapi import GraphQLRouter
 
 app = FastAPI()
 
@@ -6,3 +9,17 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@strawberry.type
+class Query:
+    @strawberry.field
+    def hello(self) -> str:
+        return "Hello World"
+
+
+schema = strawberry.Schema(Query)
+
+graphql_app = GraphQLRouter(schema)
+
+app.include_router(graphql_app, prefix="/graphql")
